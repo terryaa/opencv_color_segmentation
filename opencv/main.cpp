2599b035529f60f89101134fa51421b2c9ff6093
younghoon Kim <terryaa@daum.net>
2019-01-03 22:17:56 +0900

    Initial Commit

---
+++ opencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv
+//
+//  Created by younghoon kim on 03/01/2019.
+//  Copyright © 2019 younghoon kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
